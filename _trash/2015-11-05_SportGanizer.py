ed8fc6ab36cda036df2e465ae358c37d3c78351c
simonverret <verret.simon@gmail.com>
2015-11-05 22:07:37 -0500

    FIXED MUTABLE DEFAULT ARG in POOL __init__, + made minor modification and todo list in SingleElimination + tests for Pool

--- _trash/2015-11-05_SportGanizer.py
+++ _trash/2015-11-05_SportGanizer.py
@@ -58,8 +58,6 @@ class Team:
 	#To implement : team delete with incidence on matchs, __lt__ conditionned on selected pool, __cmp__ or __gt__
 
 
-
-
 class Match:
 	def __init__(self, teamA, teamB, name = None):
 		self.name = name
@@ -148,27 +146,32 @@ class Match:
 
 
 
+
+
 class Pool:
-	def __init__(self, initName=None, initTeamList=None):
+
+	# TODO:
+		#createTimeSlots(timeList) and assignTimeSlotsToMatches(timeSlots) OR a function that does both at same time, although that seems too large
+		#createLocations(locationList) and assignLocationsToMatches(locations) OR a function that does both at same time, although that seems too large
+
+	def __init__(self, initName = None, initTeamList = []):
 		self.name = initName
-		if initTeamList:
-			self.nOfTeams = len(initTeamList)
-			self.teamList = initTeamList
-		else:
-			self.nOfTeams = 0
-			self.teamList = []
+		self.numberOfTeams = len(initTeamList)
 		self.numberOfMatches = 0
+		self.teamList = initTeamList
 		self.matchList = []
+
 	def rename(self,name):
 		self.name = name
 		
 	def addTeam(self, newTeam):
 		if isinstance(newTeam, list):
-			self.nOfTeams += len(newTeam)
+			self.numberOfTeams += len(newTeam)
 			self.teamList.extend(newTeam)
 		else:
-			self.nOfTeams += 1
+			self.numberOfTeams += 1
 			self.teamList.append(newTeam)
+		
 	def createMatch(self, teamANumber, teamBNumber, matchName = None):
 		self.numberOfMatches += 1
 		self.newMatch = Match(self.teamList[teamANumber], self.teamList[teamBNumber], matchName)
@@ -176,39 +179,36 @@ class Pool:
 		
 	def ranking(self):
 		sortedTeamList = sorted(self.teamList)
-		return sortedTeamList	
-
+		return sortedTeamList
+	
 	# IMPORTANT: the Pool shall receive the Teams input from the GUI
-	# TODO:
-		#createTimeSlots(timeList) and assignTimeSlotsToMatches(timeSlots) OR a function that does both at same time, although that seems too large
-		#createLocations(locationList) and assignLocationsToMatches(locations) OR a function that does both at same time, although that seems too large
-
-
-
-
+	
+	
 class Tournament:
 	def __init__(self, name = None):
 		self.name = name
 		self.poolList = []
-		self.nOfPools = 0
+			
 	def rename(self, name):
 		self.name = name
+
 	def addPool(self, pool):
 		self.poolList.append(pool)
-		self.nOfPools += 1
+		
 	def removePool(self, pool):
 		self.poolList.remove(pool)
 	
-
+		
 class SingleElimination(Tournament):
 	''' Must input a pool with method setInputPool before using other functions. '''
-	def __init__(self, initPool = None):
-		Tournament.__init__(self, 'single elimination')	
-		if initPool:
-			self.inputPool = initPool
-			self.inputPool.rename("Pool_1")
-			self.addPool(self.inputPool)
+	
+	# TODO: 
+		#updatePool using the GUI
+		#add line in buildFirstPool and buildOtherPools assigning time slots and locations to matchs in each pool via pool.assignTimeSlotsToMatches and pool.assignLocationsToMatches
 
+	def __init__(self):
+		Tournament.__init__(self, 'single elimination')
+		
 	def setInputPool(self, initPool):
 		self.inputPool = initPool
 		self.inputPool.rename("Pool_1")
@@ -220,53 +220,163 @@ class SingleElimination(Tournament):
 			print(str(self.poolList[-1].teamList[0].name)+' is the winner of the Tournament!')
 			pass
 		else:
-			self.createMatchsAndNextPool()
+			self.createPoolMatchListAndPrepareNextPool()
+			self.createPoolList()
+			
+	def createPoolMatchListAndPrepareNextPool(self):
+		# Declaring variables for clearer code
+		pool = self.poolList[-1]
+		nOfTeams = len(pool.teamList)
+		self.nOfByes = (2**(int(log2(nOfTeams)+1)))%nOfTeams
+		
+		# Creating the next pool
+		nextPoolNumber = len(self.poolList)+1
+		tempPool = Pool('Pool_'+str(nextPoolNumber), []) # BUG : If i dont put (, []) to initialize the teamList, it doesnt work, WTF TABARNAK
+		
+		# Creating matches in the current pool and adding teams to the next one.
+		for i in range(self.nOfByes):
+			tempPool.addTeam(pool.teamList[i])
+		for i in range((nOfTeams-self.nOfByes)/2):
+			nextWorstTeam = nOfTeams-1-i
+			nextBestTeam = self.nOfByes + i
+			pool.createMatch(nextWorstTeam, nextBestTeam, 'Match_'+str(pool.name[-1])+'-'+str(i+1))
+			tempPool.addTeam(pool.matchList[-1].getWinner())
+		self.addPool(tempPool)
+
+	'''
+	def createPoolList(self):
+		#Determine if last pool had a power of two team number
+		if len(self.poolList) != 0:
+			self.nOfTeamsInLatestPool = len(self.poolList[-1].teamList)
+		else:
+			self.nOfTeamsInLatestPool = len(self.inputPool.teamList)
+
+		#Create Pools
+		if self.nOfTeamsInLatestPool < 2:
+			raise ValueError("Cannot create a tournament with less than 2 teams!")
+		elif self.nOfTeamsInLatestPool == 2:
+			if len(self.poolList) == 0:
+				self.createNextPool()
+				self.createPoolList()
+			elif len(self.poolList) == 1 and len(self.inputPool.teamList) > 2:
+				self.createNextPool()
+				self.createPoolList()				
+			else:
+				pass
+		else:
+			self.createNextPool()
 			self.createPoolList()
 
-	def createMatchsAndNextPool(self):
-		inPool = self.poolList[-1]
-		nOfByes = (2**(int(log2(inPool.nOfTeams)+1)))%inPool.nOfTeams
-		nOfElim = (inPool.nOfTeams-nOfByes)/2
-		
-		# make nextPool part 1
-		outPool = Pool('Pool_'+str(self.nOfPools+1))
-		for i in range(nOfByes):
-			outPool.addTeam(inPool.teamList[i])
-		# make eliminations
-		for i in range(nOfElim):
-			nextWorstTeam = inPool.nOfTeams-1-i
-			nextBestTeam = nOfByes + i
-			elimName = 'Match_'+str(inPool.name[-1])+'-'+str(i+1)
-			inPool.createMatch(nextWorstTeam, nextBestTeam, elimName)
-			# make nextPool part 2
-			outPool.addTeam(inPool.matchList[-1].getWinner())
-		# make nextPool part 3
-		self.addPool(outPool)
-		# TODO: 
-			# need to be splitted : self.createMatchs() and pool.passingTeams()
-				# however, passing teams needs to be more flexible : pool.passingTeams()
-			# should access neither pool.teamList nor pool.nOfTeams... 
-				# functions to do : pool.size() pool.match(i) pool.team(i)
-				# there may be a way to unify pool.team(i) and pool.ranking()
+	def createNextPool(self):
+		nextPoolNumber = len(self.poolList)+1
+		tempPool = Pool('Pool_'+str(nextPoolNumber), self.createNextPoolTeamList())
+		self.createNextPoolMatchList(tempPool)
+		self.addPool(tempPool)
 
+	def createNextPoolTeamList(self):
+		if len(self.poolList) == 0:
+			if not self.nInitialTeamsIsPowerOf2:
+				teamList = self.inputPool.teamList[self.nOfByes:]
+			else:
+				teamList = self.inputPool.teamList
+		elif len(self.poolList) == 1:
+			if not self.nInitialTeamsIsPowerOf2:
+				teamList = self.inputPool.teamList[:self.nOfByes]
+				teamList.extend([self.poolList[0].matchList[i].getWinner() for i in range(len(self.poolList[0].matchList))])
+			else:
+				teamList = [self.poolList[0].matchList[i].getWinner() for i in range(len(self.poolList[0].matchList))]
+		else:
+			teamList = [self.poolList[-1].matchList[i].getWinner() for i in range(len(self.poolList[-1].matchList))]
+		return teamList
+		
+	def createNextPoolMatchList(self, pool):
+		nOfTeams = len(pool.teamList)
+		if nOfTeams < 2 : 
+			raise ValueError("Cannot create a match with less than 2 teams!")
+		for i in range(nOfTeams/2):
+			nextWorstTeam = nOfTeams-1-i
+			nextBestTeam = i
+			pool.createMatch(nextWorstTeam, nextBestTeam, 'Match_'+str(pool.name[-1])+'-'+str(i+1))
+	'''
+
+	'''	
+	def buildAllPools(self):
+		self.buildFirstPool()
+		self.buildOtherPools()
+		
+	def buildFirstPool(self):
+		self.inputPool.rename("Pool_1")
+		if self.nTeamsIsPowerOf2:
+			self.createPoolMatchList(self.inputPool, self.nOfTeams)
+			self.addPool(self.inputPool)
+		else:
+			for i in range(self.nOfAdditionnalFirstPoolMatches):
+				nextWorstTeam = self.nOfTeams-1-i
+				nextBestTeam = self.nOfFirstPoolByes + i
+				self.inputPool.createMatch(nextWorstTeam, nextBestTeam, 'Match_'+str(self.inputPool.name[-1])+'-'+str(i+1))
+			#self.createPoolMatchList(LOTS OF ARGUMENTS....) or would need to just create another function for this one time use... not sure if needed
+			self.addPool(self.inputPool)
+
+	def buildOtherPools(self):
+		if self.nTeamsIsPowerOf2:
+			for layerExponent in range(self.nOfLayers-1,0,-1):
+				poolNumber = self.nOfLayers-layerExponent+1
+				nOfTeams = 2**layerExponent
+				tempTeamList = [Team('Winner-Match_'+str(poolNumber-1)+'-'+str(i+1)) for i in range(nOfTeams)]
+				tempPool = Pool('Pool_'+str(poolNumber),tempTeamList)
+				self.createPoolMatchList(tempPool, nOfTeams)
+				self.addPool(tempPool)
+		else:
+			for layerExponent in range(self.nOfLayers-1,0,-1):
+				poolNumber = self.nOfLayers-layerExponent+1
+				nOfTeams = 2**layerExponent
+				if poolNumber == 2: # First other pool must contain the teams that had a bye if there were any
+					tempTeamList = [self.inputPool.teamList[i] for i in range(self.nOfFirstPoolByes)]
+					tempTeamList = tempTeamList + [Team('Winner-Match_'+str(poolNumber-1)+'-'+str(i+1)) for i in range(self.nOfAdditionnalTeams)]
+				else:
+					tempTeamList = [Team('Winner-Match_'+str(poolNumber-1)+'-'+str(i+1)) for i in range(nOfTeams)]
+				tempPool = Pool('Pool_'+str(poolNumber), tempTeamList)
+				self.createPoolMatchList(tempPool,nOfTeams)
+				self.addPool(tempPool)
+				
+	def createPoolMatchList(self,pool,nOfTeams):
+		for i in range(nOfTeams/2):
+			nextWorstTeam = nOfTeams-1-i
+			nextBestTeam = i
+			pool.createMatch(nextWorstTeam, nextBestTeam, 'Match_'+str(pool.name[-1])+'-'+str(i+1))
+			
+	def calcNumberOfLayers(self, nOfTeams):
+		if self.nTeamsIsPowerOf2:
+			nOfLayers = log2(nOfTeams)
+			return nOfLayers
+		else:
+			nOfLayers = int(log2(nOfTeams))+1
+			return nOfLayers
+	'''
 	def show(self):
 		for pool in self.poolList:
 			print '\n'+str(pool.name)+'\n'
 			for match in pool.matchList:
 				print str(match.name)+' : '+str(match.teamA.name)+' vs '+str(match.teamB.name)+'\n'
-
-	# TODO: 
-		#updatePool using the GUI
-		#add line in buildFirstPool and buildOtherPools assigning time slots and locations to matchs in each pool via pool.assignTimeSlotsToMatches and pool.assignLocationsToMatches
-
-
-			
-if __name__ == "__main__":
 	
+	# TO BE CONTINUED 
+	'''
+	def update(self):
+		if Pool.isAllMatchPlayed():
+			rankedTeamList = pool.Ranking()
+			#...
+		else:
+			pass
+			#could display match to play
+		print 'NYI'
+		# should consult stats and adjust teams
+	'''
+				
+if __name__ == "__main__":
 	nOfTeams = int(input("How many teams are in your tournament?"))
 	thePool = Pool('inputPool', [Team('Team_'+str(i)) for i in range(1, nOfTeams+1)])
 	theTournament = SingleElimination()
 	theTournament.setInputPool(thePool)
+	#theTournament.buildAllPools()
 	theTournament.createPoolList()
 	theTournament.show()
-	
--- tests-SportGanizer.py
+++ tests-SportGanizer.py
@@ -74,7 +74,7 @@ class TestNamedTeamInitialization:
 
 
 #
-# TESTS for CLASS MATCH INITIALIZATION (implicitely testing Match.addMatch
+# TESTS for CLASS MATCH INITIALIZATION (implicitely testing Match.addMatch)
 
 class TestUnnamedMatchWithBasicTeamsInitialization:
 	def setup_method(self,method):
@@ -245,7 +245,6 @@ class Test_match_setScore_Error:
 	def test_scoreA(self):
 		with pytest.raises(ValueError):
 			self.matchAB.setScore(Team(),5)
-
 
 class Test_match_setScore_FOUR_args:
 	def setup_method(self,method):
@@ -536,51 +535,117 @@ class Test_teams_countWins_countLosses_lt_and_sort:
 		assert self.lst == self.decLst
 
 
-	
-class TestPool:
-	#Tests on class Pool
-	
+
+
+###
+###	TESTS ON CLASS POOL
+###
+
+
+#
+# TEST on POOL initialization
+
+class TestPool_initialization:
 	def setup_method(self,method):
 		self.poolParty = Pool("Party")
+	def teardown_method(self,method):
+		del self.poolParty
+
+	def test_pool_name_is_None(self):
+		assert self.poolParty.name == "Party"
+	def test_pool_nbOfTeam(self):
+		assert self.poolParty.nOfTeams == 0
+	def test_pool_initialization(self):
+		assert self.poolParty.numberOfMatches == 0
+	def test_pool_initialization(self):
+		assert self.poolParty.teamList == []
+	def test_pool_initialization(self):
+		assert self.poolParty.matchList == []
+
+class TestPool_initialization_with_team:
+	def setup_method(self,method):
 		self.teamAlice = Team("Alice")
-		self.teamBob = Team("Bob") 	
-	
+		self.poolParty = Pool("Party",[self.teamAlice])
+	def teardown_method(self,method):
+		del self.poolParty
+
+	def test_nOfTeams_increased(self):
+		assert self.poolParty.nOfTeams == 1
+	def test_pool_initialization(self):
+		assert self.poolParty.teamList == [self.teamAlice]
+
+class TestPool_addTeam:	
+	def setup_method(self,method):
+		self.teamAlice = Team("Alice")
+		self.poolParty = Pool("Party")
+		self.poolParty.addTeam(self.teamAlice)
+	def teardown_method(self,method):
+		del self.teamAlice
+		del self.poolParty
+
+	def test_nOfTeams_increased(self):
+		assert self.poolParty.nOfTeams == 1
+	def test_team_in_pool(self):
+		assert self.teamAlice in self.poolParty.teamList
+
+class TestPool_createMatch:
+	def setup_method(self,method):
+		self.teamAlice = Team("Alice")
+		self.teamBob = Team("Bob")
+		self.poolParty = Pool("Party")
+		self.poolParty.addTeam(self.teamAlice)
+		self.poolParty.addTeam(self.teamBob)
+		self.poolParty.createMatch(0, 1, "matchTest")
 	def teardown_method(self,method):
 		del self.teamAlice
 		del self.teamBob
 		del self.poolParty
 
-	def test_pool_initialization(self):
-		assert self.poolParty.name != "defaultPool"
-		assert self.poolParty.name == "Party"
-		assert self.poolParty.numberOfTeams == 0
-		assert self.poolParty.numberOfMatches == 0
-		assert self.poolParty.teamList == []
-		assert self.poolParty.matchList == []
+	def test_both_team_in_pool(self):
+		assert self.poolParty.nOfTeams == 2
+		assert self.teamAlice in self.poolParty.teamList
+		assert self.teamBob in self.poolParty.teamList
+	def test_number_of_match_increased(self):
+		assert self.poolParty.numberOfMatches == 1
+	def test_match_name(self):
+		assert self.poolParty.matchList[0].name == "matchTest"
+	def test_match_teams(self):
+		assert self.poolParty.matchList[0].teamA == self.teamAlice
+		assert self.poolParty.matchList[0].teamB == self.teamBob
 
-	def test_addTeam_and_createMatch(self):
-		#self.poolParty.addTeam(self.teamAlice)
-		#assert self.poolParty.numberOfTeams == 1
-		#assert self.poolParty.teamList.count(self.teamAlice) == 1
-		#self.poolParty.addTeam(self.teamBob)
-		#assert self.poolParty.numberOfTeams == 2
-		#self.poolParty.createMatch(0, 1, "matchTest")
-		#assert self.poolParty.numberOfMatches == 1
-		#assert self.poolParty.matchList[0].name == "matchTest"
-		#assert self.poolParty.teamList[0] == self.teamAlice
-		#assert self.poolParty.teamList[1] == self.teamBob
-		pass
-
-	def test_addTeam_with_list_arg(self):
+class TestPool_addTeam_list:
+	def setup_method(self,method):
+		self.poolParty = Pool("Party")
+		self.teamAlice = Team("Alice")
+		self.teamBob = Team("Bob")
 		lst = [self.teamAlice,self.teamBob]
 		self.poolParty.addTeam(lst)
-		print self.poolParty.teamList
+	def teardown_method(self,method):
+		del self.teamAlice
+		del self.teamBob
+		del self.poolParty
+
+	def test_both_team_in_pool(self):
+		assert self.poolParty.nOfTeams == 2
+		assert self.teamAlice in self.poolParty.teamList
+		assert self.teamBob in self.poolParty.teamList
+	def test_addTeam_with_list_order(self):
 		assert self.poolParty.teamList[0] == self.teamAlice
 		assert self.poolParty.teamList[1] == self.teamBob
 
+class TestPool_ranking:
+	def setup_method(self,method):
+		self.teamAlice = Team("Alice")
+		self.teamBob = Team("Bob") 	
+		self.teamCharlie = Team("Charlie")
+		self.poolParty = Pool("Party")
+	def teardown_method(self,method):
+		del self.teamAlice
+		del self.teamBob
+		del self.poolParty
+
 	def test_ranking(self):
 		assert self.poolParty.ranking() == self.poolParty.teamList
-
 
 
 class TestTournament:
@@ -604,6 +669,16 @@ class TestTournament:
 		self.tournamentA.addPool(self.poolA)
 		assert self.tournamentA.poolList == [self.poolA]
 		
+
+
+
+###
+###	TESTS ON CLASS SingleElimination
+###
+
+#
+# TESTS for SingleElimination
+
 class TestSingleElimination:
 	def setup_class(self):
 		self.nOfTeams = 35
